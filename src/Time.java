874491c651f4fe8e4688f0ac9edc2b1ab3af3976
tuananh1111 <nguyentuananhchuot0612@gmail.com>
2020-09-14 21:33:46 +0700

    update

--- src/Time.java
+++ src/Time.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Time {
+
     public static void main(String[] args) {
         StopWatch watch= new StopWatch();
+        Scanner input  = new Scanner(System.in);
+
+        int[] array= new int[10000];
+        int min;
+        int i,j;
+
         double beginTime= watch.start();
-        for (int i=0;i<100000;i++){
-            System.out.println(i);
+        for ( i=0;i<array.length-1;i++){
+            min=i;
+            for ( j=i+1;j<array.length;j++){
+                if(array[j]<array[min]){
+                    min=j;
+                }
+            }
+            swap(array[min],array[i]);
         }
         double stopTime= watch.stop();
         System.out.println(watch.getElapseTime(beginTime,stopTime));
+    }
+     static void swap(int a, int b){
+        int temp;
+        temp= a;
+        a=b;
+        b=temp;
     }
 }
